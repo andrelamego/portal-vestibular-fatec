8efe4f8b8cbf64b9980d9f498c992ad8caa8332c
andre <andreolamego@gmail.com>
2026-04-22 20:01:32 -0300

    Initial Commit

---
+++ src/main/java/com/lamego/portal_vestibular_fatec/PortalVestibularFatecApplication.java
@@ -0,0 +1,13 @@
+package com.lamego.portal_vestibular_fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalVestibularFatecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalVestibularFatecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lamego/portal_vestibular_fatec/PortalVestibularFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamego.portal_vestibular_fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalVestibularFatecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
