b778c147e9add4f2b38bdfdd8f19b30be7d780a8
andre <andreolamego@gmail.com>
2026-04-27 22:52:06 -0300

    feat: implementa camada de serviço, DTO e mapper para entidade Curso
    - adiciona `CursoDTO` para transferência de dados
    - cria `CursoMapper` para conversão entre entidade e DTO
    - implementa `CursoService` com métodos de salvar, buscar, listar ativos e verificar existência por nome

---
+++ src/main/java/com/lamego/portal_vestibular_fatec/curso/CursoDTO.java
@@ -0,0 +1,13 @@
+package com.lamego.portal_vestibular_fatec.curso;
+
+import com.lamego.portal_vestibular_fatec.candidato.CandidatoDTO;
+
+import java.util.List;
+
+public record CursoDTO(
+        Long id,
+        String nome,
+        boolean ativo,
+        List<CandidatoDTO> candidatos
+) {
+}
---
+++ src/main/java/com/lamego/portal_vestibular_fatec/curso/CursoMapper.java
@@ -0,0 +1,16 @@
+package com.lamego.portal_vestibular_fatec.curso;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper
+public interface CursoMapper {
+    CursoDTO toCursoDTO(Curso curso);
+
+    @Mapping(target = "id", ignore = true)
+    Curso toEntityFromDto(CursoDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityfromDto(CursoDTO dto, @MappingTarget Curso curso);
+}
